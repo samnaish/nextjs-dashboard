6c741d7a2b785410616072ced62373b78d0c86e9
Sam Naish <sam.naish92@googlemail.com>
2024-10-08 19:30:11 +0100

    adjusted how data was managed

---
+++ app/dashboard/(overview)/page.tsx
@@ -0,0 +1,31 @@
+import { Card } from '@/app/ui/dashboard/cards';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { lusit } from '@/app/ui/fonts';
+import { Suspense } from 'react';
+import { RevenueChartSkeleton, LatestInvoicesSkeleton } from '@/app/ui/skeletons';
+import CardWrapper from '@/app/ui/dashboard/cards';
+
+ 
+export default async function Page() {
+  return (
+    <main>
+      <h1 className={`${lusit.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Suspense>
+          <CardWrapper />
+        </Suspense>
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <Suspense>
+          <RevenueChart />
+        </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton/>}>
+          <LatestInvoices />
+        </Suspense>
+      </div>
+    </main>
+  );
+}
