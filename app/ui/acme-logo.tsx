b6fecc1117622ecf2bddba6f45ed65403367355d
Sam Naish <sam.naish92@googlemail.com>
2024-10-10 20:14:05 +0100

    added search bar and pagination

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -7,7 +7,7 @@ export default function AcmeLogo() {
       className={`${lusit} flex flex-row items-center leading-none text-white`}
     >
       <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[25px]">Meercat Corp</p>
+      <p className="text-[25px]">Meerkat Corp</p>
     </div>
   );
 }
